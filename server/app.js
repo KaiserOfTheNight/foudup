21c12fe2f6fc0569e85242637e8308ce1d54fb3b
kaiserofthenight <abderrazzak.elouazghi@gmail.com>
2025-07-10 16:35:14 +0200

    work

--- server/app.js
+++ server/app.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.get('/test', (req, res) => {
   res.json({ message: '✅ Test route working!' });
+});
+
+// Add this error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 export default app;
