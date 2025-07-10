630f2951b8beb49949c790e61c5256cdbcb47295
kaiserofthenight <abderrazzak.elouazghi@gmail.com>
2025-07-10 16:29:19 +0200

    work

--- server/app.js
+++ server/app.js
@@ -1,27 +1,13 @@
 import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 
-import AuthRoutes from './src/routes/auth.route.js';
-import ProjectRoutes from './src/routes/project.route.js';
-import UserRoutes from './src/routes/user.route.js';
 
 dotenv.config();
-
 const app = express();
+app.use(express.json());
 
 app.get('/test', (req, res) => {
   res.json({ message: '✅ Test route working!' });
 });
-
-
-app.use(express.json());
-app.use(cookieParser());
-
-app.use('/api/auth', AuthRoutes);
-app.use('/api/projects', ProjectRoutes);
-app.use('/api/user', UserRoutes);
-
 
 export default app;
