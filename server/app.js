ebf6bbbf5c7df54c6a84558c85c1e48814bc1a0b
kaiserofthenight <abderrazzak.elouazghi@gmail.com>
2025-07-11 13:07:03 +0200

    done

--- server/app.js
+++ server/app.js
@@ -8,6 +8,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('✅ API is running.');
+});
+
+
 app.get('/test', (req, res) => {
   res.json({ message: '✅ Test route working!' });
 });
